2e19780ba3c49cfca81e6d69c86c4309a62f6c3e
Manogya Singh <manogyasinghsuryansh@gmail.com>
2024-03-04 00:35:13 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,18 +1,26 @@
-primes=[None, None, True,True]
-def sievecontinue(primes,till):
-    till+=1
-    yet=len(primes)
-    primes+= ([True for i in range(yet,till)])
-    for i in range (2,till):
-        if primes[i]:
-            uplim=till//i+1
-            while uplim*i>=till-1:
-                uplim-=1
-            for j in range (2,uplim):
-                primes[i*j]=0
-    return primes
-
-if __name__=="__main__":
-    primes=sievecontinue(primes,10000)
-    while(True):
-        print(primes[int(input())])
+from math import sqrt
+ 
+u=3000000
+prime=[None,None]+[True]*(u-1)
+for i in range(2,len(prime)):
+    if prime[i]:
+        for j in range(i,u//i+1):
+            prime[i*j]=False
+primes=[i for i in range(u) if prime[i]]
+ 
+def primef(a):
+    if a == 1:
+        return tuple()
+    if prime[a]:
+        return (a,)
+    for i in primes:
+        if i*i>a:
+            break
+        else:
+            if a%i==0:
+                return (i,) + primef(a//i)
+anst=primef(600851475143)
+if anst=tuple():
+    print(600851475143)
+else:
+    print(anst[-1])
